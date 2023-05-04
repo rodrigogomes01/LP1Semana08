f85db6247830b5bbf395a75b05b4b067a95aa9d6
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-05-05 00:34:39 +0100

    Exercise 5
    Invoke method Move() and print Health and Cost

--- GameUnits/Program.cs
+++ GameUnits/Program.cs
@@ -6,7 +6,12 @@ namespace GameUnits
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MilitaryUnit mu = new MilitaryUnit(2, 10, 5);
+            mu.Move();
+
+            // Print health and cost
+            Console.WriteLine("Health: " + mu.Health);
+            Console.WriteLine("Cost: " + mu.Cost);
         }
     }
 }
