d7b11b8f9fc408c71577d20423f758b0aadec827
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-05-05 13:53:34 +0100

    Exercise 8
    Added public property XP with int and protected set

--- GameUnits/XPUnit.cs
+++ GameUnits/XPUnit.cs
@@ -7,5 +7,15 @@ namespace GameUnits
 {
     public abstract class XPUnit : Unit
     {
+        // Add public property XP with int and protected set
+        public int XP { get; protected set; }
+        public override string ToString()
+        {
+            return $"{base.ToString()} XP = {XP}";
+        }
+        public XPUnit(int mov, int health) : base(mov, health)
+        {
+            XP = 0;
+        }
     }
 }
