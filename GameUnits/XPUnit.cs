1736093e6cc304384be83c1e6321888b4d24e7ba
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-05-05 14:01:18 +0100

    Exercise 8
    Added method ToString() and added a constructor

--- GameUnits/XPUnit.cs
+++ GameUnits/XPUnit.cs
@@ -17,5 +17,18 @@ namespace GameUnits
         {
             XP = 0;
         }
+
+        // Constructor with Health and movement and add them to the constructor of the base class and initialize XP to 0
+        public XPUnit(int mov, int health, int xp) : base(mov, health)
+        {
+            XP = xp;
+        }
+
+        // Method ToString() that returns a string with the base class ToString() and the XP
+        public override void Move(int mov)
+        {
+            base.Move(mov);
+            XP += mov;
+        }
     }
 }
